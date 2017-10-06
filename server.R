6eeaf0c54e53fae3e554869179007ee6c7dc3dc1
Ronald Fernando Rodríguez Barbosa <ronaldraxon@gmail.com>
2017-10-05 21:30:59 -0500

    Ajustes prreliminares

--- server.R
+++ server.R
@@ -4,60 +4,18 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  mtcars$mpgsp <- ifelse(mtcars$mpg - 20 > 0, mtcars$mpg -20,0)
-  model1 <- lm(hp~mpg,data=mtcars)
-  model2 <- lm(hp~mpgsp + mpg, data = mtcars)
-  
-  model1pred <- reactive({
-    mpgInput <- input$sliderMPG
-    predict(model1,newdata= data.frame(mpg=mpgInput))
-  })
-  
-  model2pred <- reactive({
-    mpgInput <- input$sliderMPG
-    predict(model2, newdata= 
-              data.frame(mpg=mpgInput,
-                         mpgsp = ifelse(mpgInput-20 >0,
-                                        mpgInput-20,0)))
-  })
-  output$plotPrediction1 <- renderPlot({
-    mpgInput <- input$sliderMPG
-    
-    plot(mtcars$mpg, mtcars$hp, xlab = "Miles per gallon",
-         ylab = "Horsepower", bty = "n", pch =16,
-         xlim = c(10,35), ylim = c(50,350))
-    
-    if(input$showModel1){
-      abline(model1, col = "red", lwd = 2)
-    }
-    if(input$showModel2){
-      model2lines <- predict(model2, newdata = data.frame(
-        mpg = 10:35, mpgsp = ifelse(10:35 - 20 > 0, 10:35 - 20, 0) 
-      ))
-      lines(10:35, model2lines, col ="blue", lwd =2)
-    }
-    legend(25,250,c("model 1 prediction","Model 2 prediction"), pch = 16,
-           col = c("red","blue"), bty = "n", cex = 1.2)
-    points(mpgInput, model1pred(),col = "red", pch =16, cex = 2)
-    points(mpgInput, model2pred(),col = "blue", pch =16, cex = 2)
-  })
-  
-  output$pred1 <- renderText({
-    model1pred()
-  })
-  
-  output$pred2 <- renderText({
-    model2pred()
-  })
+
   
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    x <- seq(input$sliderX[1],input$sliderX[2], length= input$sliderLenght)
+    y <- seq(input$sliderY[1],input$sliderY[2], length= 30)
+    f <- function(x, y) { r <- sqrt(x^2+y^2); 10 * sin(r)/r }
+    z <- outer(x, y, f)
+    z[is.na(z)] <- 1
+    op <- par(bg = "white")
+    persp(x, y, z, theta = input$theta, phi = input$phi, 
+          expand = 0.8,ltheta = input$sliderShade, shade = 0.75, col = "lightblue")
     
   })
   
--- ui.R
+++ ui.R
@@ -4,36 +4,27 @@ library(shiny)
 shinyUI(
   fluidPage(
     # Application title
-    titlePanel("Any sample"),
+    titlePanel("3D plot parameters"),
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
       sidebarPanel(
         h4("Options bar "),
-        numericInput("numeric","How many random numbers?",value=100,min=1,max=1000,step=1),
-        sliderInput("bins","Number of bins:",min = 1,max = 50,value = 30),
-        sliderInput("sliderX","Min and max X:",min = -100,max = 100,value = c(-50,50)),
-        sliderInput("sliderY","Min and max Y",min = -100,max = 100,value = c(-50,50)),
+        numericInput("sliderLenght","Sequence lenght:",value=30,min=2,max=100,step=1),
+        sliderInput("theta","Theta rotation:",min = 1,max = 350,value = 24),
+        sliderInput("phi","Phi rotation:",min = 1,max = 350,value = 20),
+        sliderInput("sliderX","Min and max X:",min = -100,max = 100,value = c(-25,25)),
+        sliderInput("sliderY","Min and max Y",min = -100,max = 100,value = c(-25,25)),
         checkboxInput("show_xlab","show or hide x label", value=TRUE),
         checkboxInput("show_ylab","show or hide y label", value=TRUE),
         checkboxInput("show_main","show or hide main", value=TRUE),
-        sliderInput("sliderMPG","What is the MPG of the car?",min = 10,max = 35,value = 20),
-        checkboxInput("showModel1","show or hide model 1", value=TRUE),
-        checkboxInput("showModel2","show or hide model 2", value=TRUE),
-        submitButton("Submit")# New!
+        sliderInput("sliderLtheta","Ltheta",min = -180,max = 180,value = -120)
+        #submitButton("Submit")# New!
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-        h3("main panel text"),
-        plotOutput("distPlot"),
-        plotOutput("plot1"),
-        textOutput("text1"),
-        h3("Predictions"),
-        plotOutput("plotPrediction1"),
-        h3("predicted horse power model 1"),
-        textOutput("pred1"),
-        h3("predicted horse power model 2"),
-        textOutput("pred2")
+        plotOutput("distPlot",width = "100%", height = "800px")
+        
       )
     )
   )
