bedd07eb121336a9d5c7c84a687db026beb2f049
Ronald Fernando Rodríguez Barbosa <ronaldraxon@gmail.com>
2017-10-06 20:00:12 -0500

    definición final

--- server.R
+++ server.R
@@ -1,13 +1,10 @@
 
 library(shiny)
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a 3d PLot
 shinyServer(function(input, output) {
-   
-
   
-  output$distPlot <- renderPlot({
-    
+  output$plot <- renderPlot({
     x <- seq(input$sliderX[1],input$sliderX[2], length= input$sliderLenght)
     y <- seq(input$sliderY[1],input$sliderY[2], length= 30)
     f <- function(x, y) { r <- sqrt(x^2+y^2); 10 * sin(r)/r }
@@ -15,25 +12,10 @@ shinyServer(function(input, output) {
     z[is.na(z)] <- 1
     op <- par(bg = "white")
     persp(x, y, z, theta = input$theta, phi = input$phi, 
-          expand = 0.8,ltheta = input$sliderLtheta, shade = input$sliderShade, col = "lightblue")
-    
+          expand = 0.8,ltheta = input$sliderLtheta, 
+          shade = input$sliderShade, col = input$Color)
   })
   
-  output$plot1 <- renderPlot({
-    set.seed(2016-05-25)
-    number_of_points <- input$numeric
-    minX <- input$sliderX[1]
-    maxX <- input$sliderX[2]
-    minY <- input$sliderY[1]
-    maxY <- input$sliderY[2]
-    dataX <- runif(number_of_points, minX, maxX)
-    dataY <- runif(number_of_points, minY, maxY)
-    xlab <- ifelse(input$show_xlab,"X Axis","")
-    ylab <- ifelse(input$show_ylab,"Y Axis","")
-    main <- ifelse(input$show_main, "Tittle","")
-    plot(dataX,dataY,xlab =xlab,ylab=ylab, main = main,
-         xlim = c(-100,100), ylim = c(-100,100))
-  })
   
   output$text1 = renderText(input$bins)
 })
--- ui.R
+++ ui.R
@@ -8,23 +8,24 @@ shinyUI(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
       sidebarPanel(
-        h4("Options bar "),
-        numericInput("sliderLenght","Sequence lenght:",value=30,min=2,max=100,step=1),
+        h5("Set values or drag the sliders to change features or visualization on the 3d plot."),
+        numericInput("sliderLenght","Sequence lenght:",value=30,min=2,max=500,step=1),
         sliderInput("theta","Theta rotation:",min = 1,max = 350,value = 24),
         sliderInput("phi","Phi rotation:",min = 1,max = 350,value = 20),
         sliderInput("sliderX","Min and max X:",min = -100,max = 100,value = c(-25,25)),
         sliderInput("sliderY","Min and max Y",min = -100,max = 100,value = c(-25,25)),
         sliderInput("sliderLtheta","Ltheta",min = -180,max = 180,value = -120),
         sliderInput("sliderShade","Shade",min = 0,max = 3,value = 0.75,step=0.1),
-        checkboxInput("show_xlab","show or hide x label", value=TRUE),
-        checkboxInput("show_ylab","show or hide y label", value=TRUE),
-        checkboxInput("show_main","show or hide main", value=TRUE)
+        selectInput("Color", "Set a color:",
+                    c("Light blue" = "lightblue",
+                      "Red" = "red",
+                      "Green" = "green"))
         #submitButton("Submit")# New!
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-        plotOutput("distPlot",width = "100%", height = "800px")
+        plotOutput("plot",width = "100%", height = "800px")
         
       )
     )
